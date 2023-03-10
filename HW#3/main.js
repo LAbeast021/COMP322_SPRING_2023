2ac44a4a7a14a6aaf67add0045363997e5e60cc5
LAbeast021 <kiarashhesampour@gmail.com>
2023-03-10 14:49:04 -0800

    second commit

--- HW#3/main.js
+++ HW#3/main.js
@@ -9,15 +9,14 @@ $("#Form").submit(function(evt) {
         processTime : evt.target[1].value
     }
     processHolder.push(process);
-    console.log(processHolder);
     processNumber += 1 ;
     evt.target.reset(); 
+    render();
 })
 
 function render () {
     if (processNumber <= 5 ) {
-        console.log($("#process_input_header")[0] , processNumber)
-        $("#process_input_header")[0].innerText = " sag" 
+        $("#process_input_header")[0].innerText = ` Please Enter Process # ${processNumber} ` ;
     }
 }
 
