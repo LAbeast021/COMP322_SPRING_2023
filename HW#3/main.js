e05be099ea999555d963f6f5d8ebb02c2fc1b53a
LAbeast021 <kiarashhesampour@gmail.com>
2023-03-10 14:54:01 -0800

    organizing

--- HW#3/main.js
+++ HW#3/main.js
@@ -2,7 +2,11 @@ var processHolder = [];
 var processNumber = 1 ;
 
 // _----_----_-----_------_------___------ Query Selecectors _----_----_-----_------_------___------ // 
-$("#Form").submit(function(evt) {
+const processAddForm = $("#Form") ;
+
+
+// _----_----_-----_------_------___------ Event Listener _----_----_-----_------_------___------ // 
+processAddForm.submit(function(evt) {
     evt.preventDefault();
     let process = { 
         arrivalTime : evt.target[0].value ,
@@ -14,9 +18,14 @@ $("#Form").submit(function(evt) {
     render();
 })
 
+// _----_----_-----_------_------___------ Functions  _----_----_-----_------_------___------ // 
+
 function render () {
     if (processNumber <= 5 ) {
         $("#process_input_header")[0].innerText = ` Please Enter Process # ${processNumber} ` ;
+    }
+    else {
+
     }
 }
 
